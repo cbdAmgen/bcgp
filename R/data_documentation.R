8069c69116359f5c7ae05e82e4563cfd5adb9aac
cbdAmgen <42254515+cbdAmgen@users.noreply.github.com>
2020-01-30 09:38:54 -0800

    still messing with data

---
+++ R/data_documentation.R
@@ -0,0 +1,39 @@
+#' Data from a non-stationary function
+#'
+#' A dataset containing data simulated from a non-stationary function used in
+#' Xiong (2007) and Ba and Joseph (2009)
+#'
+#' @format A list with components for training data and test data:
+#' \describe{
+#'   \item{x}{training data locations}
+#'   \item{y}{training data values}
+#'   \item{x_test}{test data locations}
+#'   \item{y_test}{test data values}
+#' }
+"BJX"
+
+#' Data from the Wing Weight example
+#'
+#' A dataset containing data for the wing weight of a light aircraft
+#'
+#' @format A list with components for training data and test data:
+#' \describe{
+#'   \item{x}{training data locations}
+#'   \item{y}{training data values}
+#'   \item{x_test}{test data locations}
+#'   \item{y_test}{test data values}
+#' }
+"WingWeight"
+
+#' Data from the Heat Exchange example
+#'
+#' A dataset containing data simulated from a heat exchange simulator
+#'
+#' @format A list with components for training data and test data:
+#' \describe{
+#'   \item{x}{training data locations}
+#'   \item{y}{training data values}
+#'   \item{x_test}{test data locations}
+#'   \item{y_test}{test data values}
+#' }
+"HeatExchange"
